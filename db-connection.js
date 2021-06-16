d8d2f5b7aea3f4eaba5375ae424dfe03b3cc3612
R on <82355929+rbill314@users.noreply.github.com>
2021-06-16 02:21:37 +0000

    added commit

--- db-connection.js
+++ db-connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const db = mongoose.connect(process.env.DB, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 
 module.exports = db
